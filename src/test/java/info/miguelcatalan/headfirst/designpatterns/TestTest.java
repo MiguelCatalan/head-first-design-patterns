5573a7e11a35af3a0fd31c1aacb6f9da3969064c
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2016-06-27 22:40:44 +0200

    First commit

---
+++ src/test/java/info/miguelcatalan/headfirst/designpatterns/TestTest.java
@@ -0,0 +1,14 @@
+package info.miguelcatalan.headfirst.designpatterns;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestTest {
+
+    @Test
+    public void shouldTestsIfTestWorks() {
+        assertTrue(true);
+    }
+}
