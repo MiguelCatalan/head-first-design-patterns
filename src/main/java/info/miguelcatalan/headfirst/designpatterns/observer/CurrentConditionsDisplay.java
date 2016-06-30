e9940818e7adb1187e91d46591c14ccf5479d589
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2016-06-30 20:49:56 +0200

    Adds implementation of the observer pattern

---
+++ src/main/java/info/miguelcatalan/headfirst/designpatterns/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,29 @@
+package info.miguelcatalan.headfirst.designpatterns.observer;
+
+class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public CurrentConditionsDisplay(Subject subject) {
+        subject.registerObserver(this);
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+    }
+}
---
+++ src/main/java/info/miguelcatalan/headfirst/designpatterns/observer/Observer.java
@@ -0,0 +1,5 @@
+package info.miguelcatalan.headfirst.designpatterns.observer;
+
+interface Observer {
+    void update(float temperature, float humidity, float pressure);
+}
